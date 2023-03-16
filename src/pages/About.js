115226b00beedfcff0e6fd2a29118d25e4b69f37
Margaret Osei <margaretosei@Margarets-iMac.local>
2023-03-16 14:29:57 +0000

    adding lastest changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import "../App.css";
 import { useState } from "react";
 import Layout from "../components/Layout";
+import AboutPageContent from "../components/AboutPageContent";
 
 function About() {
   const [framework, setFramework] = useState("");
@@ -9,37 +10,35 @@ function About() {
       <Layout>
         <div className="container container-about">
           <h1 className="tag-line">
-            Music is the mediator of the spiritual and{" "}
+            Music is the mediator of the <br />
+            spiritual and Soul{" "}
           </h1>
-          <div className="about-content">
-            <h3>Music is the Way</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-              sodales orci sapien, id elementum diam dapibus in. Sed bibendum
-              quis arcu sed varius. Nullam quis{" "}
-            </p>
-            <img className="about-image" src="../images/aboutpic-singing.jpg"></img>
-          </div>
-          <div className="about-content">
-            <h3>Music is the Way</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-              sodales orci sapien, id elementum diam dapibus in. Sed bibendum
-              quis arcu sed varius. Nullam quis{" "}
-            </p>
-            <img className="about-image" src="../images/aboutpic-singing.jpg"></img>
-          </div>
-          <div className="about-content">
-            <h3>Music is the Way</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-              sodales orci sapien, id elementum diam dapibus in. Sed bibendum
-              quis arcu sed varius. Nullam quis{" "}
-            </p>
-            <img className="about-image" src="../images/aboutpic-singing.jpg"></img>
-          </div>
-          
-
+          <AboutPageContent
+            title="Music is the Way"
+            number="1"
+            paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+          sodales orci sapien, id elementum diam dapibus in. Sed bibendum
+          quis arcu sed varius. Nullam quis"
+            imageUrl="../images/aboutpic-singing.jpg"
+          />
+          <AboutPageContent
+            title="Music is the Way"
+            number="2"
+            paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+          sodales orci sapien, id elementum diam dapibus in. Sed bibendum
+          quis arcu sed varius. Nullam quis"
+            imageUrl="../images/aboutpic-singing.jpg"
+            classCustom="about-content-2"
+          />
+          <AboutPageContent
+            title="Music is the Way"
+            number="3"
+            paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+          sodales orci sapien, id elementum diam dapibus in. Sed bibendum
+          quis arcu sed varius. Nullam quis"
+            imageUrl="../images/aboutpic-singing.jpg"
+        
+          />
         </div>
       </Layout>
     </div>
